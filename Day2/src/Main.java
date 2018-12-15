0a22e7706da22347bedb63fea0af9e9e27ac663d
bgongre <27456325+bgongre@users.noreply.github.com>
2018-12-14 20:22:59 -0500

    Advent Day2 Solved

--- Day2/src/Main.java
+++ Day2/src/Main.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Reader r1 = new Reader();
-        r1.getIdNumbers("C:\\Users\\TJ3314\\Desktop\\Code\\AdventOfCode\\Day2\\src\\checksum.txt");
+        r1.getIdNumbers("...\\AdventOfCode2018\\Day2\\Day2\\src\\checksum.txt");
         r1.createMapArray();
         r1.calculateCheckSum();
     }
@@ -38,29 +38,33 @@ class Reader {
     public void createMapArray() {
         for (int i = 0; i < idNumbers.size(); i++) {
             String idNum = idNumbers.get(i);
-            System.out.println(idNum);
             for (int j = 0; j < idNum.length(); j++) {
                 char c = idNum.charAt(j);
-                System.out.println(c);
                 Integer count = map.get(c);
                 if (count == null) {
-                    map.put(c, 0);
+                    map.put(c, 1);
                 } else {
                     map.put(c, count + 1);
                 }
-                System.out.println(map);
             }
-//            System.out.println(map);
+            if (charCount.stream().anyMatch(n -> n == 3)) {
+                tripleLetter++;
+            }
+
+            if (charCount.stream().anyMatch(n -> n == 2)) {
+                doubleLetter++;
+            }
+            map.clear();
         }
+
     }
 
-        public int calculateCheckSum () {
-            int result = doubleLetter * tripleLetter;
-            System.out.println("Triple: " + tripleLetter);
-            System.out.println("Double: " + doubleLetter);
-            System.out.println("Total : " + result);
-            return result;
-        }
-   }
 
-
+    public int calculateCheckSum() {
+        int result = doubleLetter * tripleLetter;
+        System.out.println("Triple: " + tripleLetter);
+        System.out.println("Double: " + doubleLetter);
+        System.out.println("Total : " + result);
+        return result;
+    }
+}
